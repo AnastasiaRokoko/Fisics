f9ba08c85a496fd5b4a623a6ec73b3eb3c46f426
Anastasia <Anastasia@LAPTOP-63ROBSIB>
2023-02-01 17:54:13 +0300

    Добавьте файлы проекта.

---
+++ Переделанные проекты с VB/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Переделанные_проекты_с_VB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 257);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 35);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(226, 142);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 33);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(226, 190);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 33);
+            this.textBox2.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(39, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(167, 23);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Сопротивление (Ом):";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(46, 321);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(255, 75);
+            this.label4.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(39, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 23);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Напряжение (Вольт):";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(49, 17);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(52, 20);
+            this.radioButton1.TabIndex = 17;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ток";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(49, 52);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(110, 20);
+            this.radioButton2.TabIndex = 18;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Напряжение";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(49, 90);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(132, 20);
+            this.radioButton3.TabIndex = 19;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Сопротивление";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(364, 407);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ Переделанные проекты с VB/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Переделанные_проекты_с_VB
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyUp += new KeyEventHandler(this.textBox1_TextChanged);
+            this.Text = "Закон Ома";
+           
+
+        }
+        public void Calculate()
+        {
+            float I, U, R;//ток, напряжение, сопротивление
+            if (radioButton1.Checked == true)
+            {
+                //ток
+                U = float.Parse(textBox1.Text);
+                R= float.Parse(textBox2.Text);
+                if (R != 0)
+                {
+                    I = U / R;
+                    label4.Text = string.Format("Ток: {0:f2} A", I);
+                }
+                else
+                        label4.Text = string.Format("Сопротивление не должно быть равно нулю");
+                
+            }
+            if (radioButton2.Checked == true)
+            {
+                //напряжение
+                I = float.Parse(textBox1.Text);
+                R = float.Parse(textBox2.Text);
+                U = I * R;
+                label4.Text = string.Format("Напряжение: {0:f2} B", U);
+
+            }
+            if (radioButton3.Checked == true)
+            {
+                //сопротивление
+                U = float.Parse(textBox1.Text);
+                I = float.Parse(textBox2.Text);
+                if (I != 0)
+                {
+                   R=U/I;
+                    label4.Text = string.Format("Сопротивление: {0:f2} Ом", R);
+                }
+                else
+                    label4.Text = string.Format("Ток не должнен быть равен нулю");
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((textBox1.Text != "") & (textBox2.Text != "")) Calculate();
+            else label4.Text = "Нужно ввести исходные данные в оба поля";
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Text = "Напряжение (Вольт):";
+            label3.Text = "Сопротивление (Ом):";
+            label4.Text = "";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Text = "Ток (Ампер):";
+            label3.Text = "Сопротивление (Ом):";
+            label4.Text = "";
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Text = "Напряжение (Вольт):";
+            label3.Text = "Ток (Ампер):";
+            label4.Text = "";
+        }
+
+        private void textBox1_TextChanged(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                textBox2.Focus();
+                textBox2_TextChanged(sender, e);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, KeyEventArgs e)
+        {
+            if (textBox2.Text != "")
+                button1.Focus();
+        }
+    }
+}
+
